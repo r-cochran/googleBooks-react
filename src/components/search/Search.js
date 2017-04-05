48ef1019b3f09f77e68b90f9ba56bdf9b7458a00
Robert Cochran <rcochran@manifestcorp.com>
2017-04-04 21:27:03 -0400

    wired up the button behavior

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -9,27 +9,33 @@ class Search extends Component {
   constructor(props){
     super(props);
     this.state = {
-      books: []
+      books: [],
+      searchText: ""
     };
   }
-  search(event) {
+  handleKeyPress(event){
     let self = this;
     let value = event.target.value;
+    this.setState({searchText: value})
     if(event.key === "Enter" && value){
-      fetch('https://www.googleapis.com/books/v1/volumes?q=' + value, {
+      self.search()
+    }
+  }
+  search() {
+    let self = this
+    fetch('https://www.googleapis.com/books/v1/volumes?q=' + self.state.searchText, {
         method: "GET",
         dataType: 'json'
       })
       .then((r) => r.json())
       .then(books => self.setState({books: books.items}) )
-    }
   }
 
   render(){
     return (<Row>
               <Col md={12}>
-                <FormControl type="text" id='searchText' placeholder="Search" onKeyPress={this.search.bind(this)}/> 
-                <Button id="search" bsStyle="primary"><FontAwesome name='search' /> Search</Button>
+                <FormControl type="text" id='searchText' placeholder="Search" onKeyPress={this.handleKeyPress.bind(this)}/> 
+                <Button id="search" bsStyle="primary" onClick={this.search.bind(this)}><FontAwesome name='search' /> Search</Button>
                 <BookResultsList books={this.state.books}></BookResultsList>
               </Col>
             </Row>)
