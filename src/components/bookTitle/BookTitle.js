eaffcb16bfd1eec01d9782594d3d6031ee9aae5b
Robert Cochran <rcochran@manifestcorp.com>
2017-04-04 21:43:28 -0400

    fixed issue where undefined titles and authors were throwing errors during rendering book titles.

--- src/components/bookTitle/BookTitle.js
+++ src/components/bookTitle/BookTitle.js
@@ -14,8 +14,8 @@ class BookTitle extends Component {
         <span className="title">{this.props.title}</span> 
       </Col>
       <Col md={4}>
-        <div className="authors">{this.props.authors.join(", ")}</div>
-        <div className="publishers">{this.props.publisher} ({this.props.publishedDate})</div>
+        <div className="authors">{(this.props.authors) ? this.props.authors.join(", ") : ""}</div>
+        <div className="publishers">{this.props.publisher} {(this.props.publishedDate) ? "(" + this.props.publishedDate + ")": ""}</div>
       </Col>
     </Row>
     )
